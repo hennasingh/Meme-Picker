86f991dbf246c866df96334cc996b4930c4a4917
Henna Singh <hennasingh04@gmail.com>
2023-10-06 22:41:08 +0100

    added logic to select and display image of selected emoji cat.

--- index.js
+++ index.js
@@ -1,8 +1,61 @@
 import { catsData } from "./data.js"
 
 const emotionsDiv = document.getElementById('emotion-radios')
+const imageBtn = document.getElementById('get-image-btn')
+const gifsOnlyOption = document.getElementById('gifs-only-option')
+const memeModalInner = document.getElementById('meme-modal-inner')
+const memeModal = document.getElementById('meme-modal')
+const memeModalCloseBtn = document.getElementById('meme-modal-close-btn')
+
+imageBtn.addEventListener('click', renderCat)
 
 emotionsDiv.addEventListener('change', highlightCheckedOption)
+
+memeModalCloseBtn.addEventListener('click', closeModal)
+
+function closeModal() {
+    memeModal.style.display = "none"
+}
+
+
+function getMatchingCatsArray() {
+    if(document.querySelector('input[type="radio"]:checked')) {
+        const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
+        const matchingCatsArray = catsData.filter(function(cat) {
+            if(gifsOnlyOption.checked) {
+                return cat.emotionTags.includes(selectedEmotion) && cat.isGif
+            } else {
+                return cat.emotionTags.includes(selectedEmotion)
+            }
+        })
+        return matchingCatsArray
+    }
+}
+
+function renderCat() {
+    const cat = getSingleCatObject()
+    memeModalInner.innerHTML = `
+        <img 
+            src="./images/${cat.image}"
+            class="cat-img"
+            alt="${cat.alt}"
+        >
+    `
+    memeModal.style.display = "flex"
+
+}
+
+function getSingleCatObject() {
+    const catsArray = getMatchingCatsArray()
+
+    if(catsArray.length === 1){
+        return catsArray[0]
+    } else {
+        return catsArray[Math.floor(Math.random() * catsArray.length)]
+    }
+}
+
+
 
 function highlightCheckedOption(event) {
 
